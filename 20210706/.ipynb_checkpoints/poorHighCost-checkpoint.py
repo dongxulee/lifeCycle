2b4a74dbd64357d00b29f7d946a66afcba747cc6
dongxulee <leodongxu@gmail.com>
2021-07-04 13:13:40 -0400

    risk aversion 4

--- 20210706/.ipynb_checkpoints/poorHighCost-checkpoint.py
+++ 20210706/.ipynb_checkpoints/poorHighCost-checkpoint.py
@@ -14,7 +14,7 @@ T_R = 45
 # discounting factor
 beta = 1/(1+0.02)
 # utility function parameter 
-gamma = 2
+gamma = 4
 # relative importance of housing consumption and non durable consumption 
 alpha = 0.7
 # parameter used to calculate the housing consumption 
